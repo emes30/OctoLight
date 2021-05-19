74b5990f00b3a546b4d0f31b9afdfce95cfa1c22
emes30 osm <emes30@o2.pl>
2021-05-19 12:58:12 +0200

    added home assistant support

--- octoprint_octolight/__init__.py
+++ octoprint_octolight/__init__.py
@@ -1,126 +1,268 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+import threading
 import octoprint.plugin
 from octoprint.events import Events
 import flask
+import requests
 
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    GPIO_OK = True
+except:
+    GPIO_OK = False
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+if GPIO_OK:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
 
 class OctoLightPlugin(
-		octoprint.plugin.AssetPlugin,
-		octoprint.plugin.StartupPlugin,
-		octoprint.plugin.TemplatePlugin,
-		octoprint.plugin.SimpleApiPlugin,
-		octoprint.plugin.SettingsPlugin,
-		octoprint.plugin.EventHandlerPlugin,
-		octoprint.plugin.RestartNeedingPlugin
-	):
+        octoprint.plugin.AssetPlugin,
+        octoprint.plugin.StartupPlugin,
+        octoprint.plugin.TemplatePlugin,
+        octoprint.plugin.SimpleApiPlugin,
+        octoprint.plugin.SettingsPlugin,
+        octoprint.plugin.EventHandlerPlugin,
+        octoprint.plugin.RestartNeedingPlugin
+    ):
 
-	light_state = False
+    def __init__(self):
+        self._check_light_state_thread = None
+        self._check_light_state_event = threading.Event()
+        self._light_state = False
 
-	def get_settings_defaults(self):
-		return dict(
-			light_pin = 13,
-			inverted_output = False
-		)
+    def get_settings_defaults(self):
+        return dict(
+            light_pin = 13,
+            inverted_output = False,
+            light_device = "GPIO"
+        )
 
-	def get_template_configs(self):
-		return [
-			dict(type="navbar", custom_bindings=True),
-			dict(type="settings", custom_bindings=True)
-		]
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=True),
+            dict(type="settings", custom_bindings=True)
+        ]
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/octolight.js"],
-			css=["css/octolight.css"],
-			#less=["less/octolight.less"]
-		)
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/octolight.js"],
+            css=["css/octolight.css"],
+            #less=["less/octolight.less"]
+        )
 
-	def on_after_startup(self):
-		self.light_state = False
-		self._logger.info("--------------------------------------------")
-		self._logger.info("OctoLight started, listening for GET request")
-		self._logger.info("Light pin: {}, inverted_input: {}".format(
-			self._settings.get(["light_pin"]),
-			self._settings.get(["inverted_output"])
-		))
-		self._logger.info("--------------------------------------------")
+    def register_custom_events(self):
+        return ["light_state_changed"]
 
-		# Setting the default state of pin
-		GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
-		if bool(self._settings.get(["inverted_output"])):
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
-		else:
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
-
-		#Because light is set to ff on startup we don't need to retrieve the current state
-		"""
-		r = self.light_state = GPIO.input(int(self._settings.get(["light_pin"])))
-        if r==1:
-                self.light_state = False
-        else:
-                self.light_state = True
-
-        self._logger.info("After Startup. Light state: {}".format(
-                self.light_state
+    def on_after_startup(self):
+        self._light_state = False
+        self._logger.info("--------------------------------------------")
+        self._logger.info("OctoLight started, listening for GET request")
+        self._logger.info("Light pin: {}, inverted_input: {}".format(
+            self._settings.get(["light_pin"]),
+            self._settings.get(["inverted_output"])
         ))
+        self._logger.info("--------------------------------------------")
+
+        # Setting the default state of pin
+        if GPIO_OK:
+              GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
+              if bool(self._settings.get(["inverted_output"])):
+                GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
+              else:
+                GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
+
+        self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self._light_state))
+
+        self._check_light_state_thread = threading.Thread(target=self._check_light_state)
+        self._check_light_state_thread.daemon = True
+        self._check_light_state_thread.start()
+
+    def _gpio_toggle(self, light_state):
+        GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
+        if light_state ^ self._settings.get(["inverted_output"]):
+            GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
+        else:
+            GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
+
+    def _ha_toggle(self, light_state):
+        if light_state:
+            self.turn_light_on()
+        else:
+            self.turn_light_off()
+
+    def on_api_get(self, request):
+        self._light_state = not self._light_state
+
+        light_device = self._settings.get(["light_device"])
+        if light_device == "GPIO" and GPIO_OK:
+            self._gpio_toggle(self._light_state)
+
+        if light_device == "HA":
+            self._ha_toggle(self._light_state)
+
+        self._logger.info("Got request. Light state: {}".format(
+            self._light_state
+        ))
+
+        self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self._light_state))
+
+        return flask.jsonify(status="ok")
+
+    def on_event(self, event, payload):
+        if event == Events.CLIENT_OPENED:
+            self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self._light_state))
+            return
+
+    def get_update_information(self):
+        return dict(
+            octolight=dict(
+                displayName="OctoLight",
+                displayVersion=self._plugin_version,
+
+                type="github_release",
+                current=self._plugin_version,
+
+                user="gigibu5",
+                repo="OctoLight",
+                pip="https://github.com/gigibu5/OctoLight/archive/{target}.zip"
+            )
+        )
+
+    def send(self, cmd, data=None):
         """
+        Send request via Home Assistant API
 
-		self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
+        Args:
+            cmd (str): command to execute
+            data (str, optional): command arguments. Defaults to None.
 
-	def on_api_get(self, request):
-		# Sets the GPIO every time, if user changed it in the settings.
-		GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
+        Returns:
+            bool: request send result
+        """
+        url = self._settings.get(['address']) + '/api' + cmd
 
+        headers = dict(Authorization='Bearer ' + self._settings.get(['api_key']))
 
-		self.light_state = not self.light_state
+        response = None
+        verify_certificate = self._settings.get(['verify_certificate'])
+        try:
+            if data:
+                response = requests.post(url, headers=headers, data=data, verify=verify_certificate)
+            else:
+                response = requests.get(url, headers=headers, verify=verify_certificate)
+        except (
+                requests.exceptions.InvalidURL,
+                requests.exceptions.ConnectionError
+        ):
+            self._logger.error("Unable to communicate with server. Check settings.")
+        except Exception:
+            self._logger.exception("Exception while making API call")
+        else:
+            if data:
+                self._logger.debug("cmd={}, data={}, status_code={}, text={}".format(cmd, data, response.status_code, response.text))
+            else:
+                self._logger.debug("cmd={}, status_code={}, text={}".format(cmd, response.status_code, response.text))
 
-		# Sets the light state depending on the inverted output setting (XOR)
-		if self.light_state ^ self._settings.get(["inverted_output"]):
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
-		else:
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
+            if response.status_code == 401:
+                self._logger.warning("Server returned 401 Unauthorized. Check API key.")
+                response = None
+            elif response.status_code == 404:
+                self._logger.warning("Server returned 404 Not Found. Check Entity ID.")
+                response = None
 
-		self._logger.info("Got request. Light state: {}".format(
-			self.light_state
-		))
+        return response
 
-		self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
+    def _entity_id(self):
+        _entity_id = self._settings.get(['entity_id'])
+        _domainsplit = _entity_id.find('.')
+        if _domainsplit < 0:
+            _domain = 'light'
+            _entity_id = _domain + '.' + _entity_id
+        else:
+            _domain = _entity_id[:_domainsplit]
+        return _domain, _entity_id
 
+    def change_light_state(self, state):
+        _domain, _entity_id = self._entity_id()
 
-		return flask.jsonify(status="ok")
+        if state:
+            cmd = '/services/' + _domain + '/turn_' + state
+        else:
+            cmd = '/services/' + _domain + '/toggle'
+        data = '{"entity_id":"' + _entity_id + '"}'
+        self.send(cmd, data)
 
-	def on_event(self, event, payload):
-		if event == Events.CLIENT_OPENED:
-			self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
-			return
+    def turn_light_on(self):
+        self._logger.debug("Turn the light on")
+        self.change_light_state('on')
 
-	def get_update_information(self):
-		return dict(
-			octolight=dict(
-				displayName="OctoLight",
-				displayVersion=self._plugin_version,
+    def turn_light_off(self):
+        self._logger.debug("Turn the light off")
+        self.change_light_state('off')
 
-				type="github_release",
-				current=self._plugin_version,
+    def get_light_state(self):
+        _domain, _entity_id = self._entity_id()
 
-				user="gigibu5",
-				repo="OctoLight",
-				pip="https://github.com/gigibu5/OctoLight/archive/{target}.zip"
-			)
-		)
+        cmd = '/states/' + _entity_id
+
+        response = self.send(cmd)
+        if not response:
+            return False
+        data = response.json()
+
+        status = None
+        try:
+            status = (data['state'] == 'on')
+        except KeyError:
+            pass
+
+        if status == None:
+            self._logger.error("Unable to determine status. Check settings.")
+            status = False
+
+        return status
+
+    def _check_light_state(self):
+        while True:
+            old_state = self._light_state
+
+            self._logger.debug("Polling light state...")
+
+            if self._settings.get(["light_device"]) == 'GPIO' and GPIO_OK:
+                r = False
+                try:
+                    r = GPIO.input(int(self._settings.get(["light_pin"]))) ^ self._settings.get(["inverted_output"])
+                except Exception:
+                    self._logger.exception("Exception while reading GPIO line")
+
+                self._logger.debug("Result: {}".format(r))
+
+                self._light_state = r
+
+            if self._settings.get(["light_device"]) == 'HA':
+                self._light_state = self.get_light_state()
+
+            self._logger.debug("light_state: {}".format(self._light_state))
+
+            if (old_state != self._light_state):
+                self._logger.debug("Light state changed, firing psu_state_changed event.")
+
+                event = Events.PLUGIN_OCTOLIGHT_LIGHT_STATE_CHANGED
+                self._event_bus.fire(event, payload=dict(isLightOn=self._light_state))
+                self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self._light_state))
+
+            self._check_light_state_event.wait(5)
+            self._check_light_state_event.clear()
+
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoLightPlugin()
 
 __plugin_hooks__ = {
-	"octoprint.plugin.softwareupdate.check_config":
-	__plugin_implementation__.get_update_information
+    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+    "octoprint.events.register_custom_events": __plugin_implementation__.register_custom_events
 }
